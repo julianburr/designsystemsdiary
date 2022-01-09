2e59af0455f1f600ef697c36dec2801bc931acd7
Julian Burr <hello@julianburr.de>
2022-01-09 18:56:13 +1000

    fix: update script usage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import Script from "next/script";
 
 import { usePageView } from "src/utils/ga";
 
@@ -6,7 +7,29 @@ import "src/assets/fonts/staatliches.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   usePageView();
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* Add Google Analytics */}
+      {/* See https://nextjs.org/docs/messages/next-script-for-ga */}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+
+      {/* Main app */}
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,6 @@
 import { ComponentType } from "react";
 import { ServerStyleSheet } from "styled-components";
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document from "next/document";
 
 export default class MyDocument extends Document {
   // The below ensures SSR for styled components works as expected
@@ -28,35 +28,5 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
-  }
-
-  render() {
-    return (
-      <Html>
-        <Head>
-          {/* Add Google Analytics */}
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                  page_path: window.location.pathname,
-                });
-              `,
-            }}
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
   }
 }
